ca977ddf388b901928ddd98dd33922aa1b7bdff9
rascle <ryan.bascle@gmail.com>
2015-10-08 13:51:08 -0500

    Functional stack.js - complete up to last test.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,15 +1,24 @@
 var Stack = function(){
   var someInstance = {};
   var num = 0;
+  
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value){
-    return num;
+    storage[num] = value;
+    num++;
   };
 
   someInstance.pop = function(){
+    if (num > 0) {
+      var popped = storage[num]; 
+      conosle.log(storage[num]);
+      delete storage[num];
+      num--;
+      return popped;
+    }
   };
 
   someInstance.size = function(){
