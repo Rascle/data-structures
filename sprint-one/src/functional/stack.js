6f814d53efddb483920f2157e2483c0b3a991264
rascle <ryan.bascle@gmail.com>
2015-10-08 13:22:58 -0500

    Completed first two tests of Functional Stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,19 @@
 var Stack = function(){
   var someInstance = {};
-
+  var num = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value){
+    return num;
   };
 
   someInstance.pop = function(){
   };
 
   someInstance.size = function(){
+    return num;
   };
 
   return someInstance;
