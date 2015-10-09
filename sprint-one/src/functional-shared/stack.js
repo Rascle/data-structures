b418105b9fd23de73f348e540b05f4fd7ac3c3e6
rascle <ryan.bascle@gmail.com>
2015-10-09 00:13:00 -0500

    Completed Functional-Shared stacks.js

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.num = 0;
+  _.extend(someInstance, stackMethods);
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  //This is a method container - all instances share these methods through the extend function
+};
 
+stackMethods.push = function (value) {
+  this.num++;
+  this.storage[this.num] = value;
+}
 
+stackMethods.pop = function () {
+  if (this.num > 0) {
+    var popped = this.storage[this.num];
+    delete this.storage[this.num];
+    this.num--;
+    return popped; 
+  }
+}
+
+stackMethods.size = function () {
+  return this.num;
+}
